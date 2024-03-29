62f7d8c7e457f08f0d12e76f4e166d96fb8fb12d
Vivek D <vivek01110110@gmail.com>
2024-03-29 11:08:38 +0000

    modified:   myApp.js

--- myApp.js
+++ myApp.js
@@ -1,9 +1,32 @@
 let express = require('express');
 let app = express();
+let dotenv = require('dotenv')
 
+dotenv.config();
 
+// console.log("Hello World");                 
 
+const htmlPath= __dirname + "/views/index.html";
+app.get("/", (req,res)=>{
+    // res.send("Hello Express");
+    res.sendFile(htmlPath);
+})
 
+const cssPath = __dirname + "/public/"
+app.use("/public",express.static(cssPath));
+
+const message= {"message":"Hello json"};
+app.get("/json", (req,res)=>{
+    // res.json(message);
+    if(process.env.MESSAGE_STYLE === "uppercase")
+    {
+        res.json({"message":"HELLO JSON"});
+    }
+    else
+    {
+        res.json({"message":"Hello json"});
+    }
+});
 
 
 
