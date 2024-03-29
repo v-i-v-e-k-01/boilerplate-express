6428f5d324cba226030aeda433b9d5d80449a104
Vivek D <91778440+v-i-v-e-k-01@users.noreply.github.com>
2024-03-30 00:23:06 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,10 +1,22 @@
 let express = require('express');
 let app = express();
-let dotenv = require('dotenv')
+let dotenv = require('dotenv');
+let bodyParser = require("body-parser");
 
 dotenv.config();
 
 // console.log("Hello World");   
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+// app.post("/", (req,res)=>{
+//     const first=req.body.first;
+//     const last=req.body.last;
+
+//     res.send("firstname :${first} , lastname: ${last} ");
+// });
+
+app.use(bodyParser.json());
 
 app.use(function(req,res,next){
     console.log(req.method + " " + req.path + " - " + req.ip);
@@ -51,9 +63,12 @@ app.get("/name", (req,res)=>{
     res.send({"name":req.query.first + " " + req.query.last});
 });
 
+app.post("/name",(req,res)=>{
+    const first=req.body.first;
+    const last=req.body.last;
 
-
-
+    res.send({"name": first + " " + last});
+})
 
 
 
